205c826f9f3be241c4ff94a07b9dcd7c28f7cb73
shenwanxiang <shenwanxiang@tsinghua.org.cn>
2022-10-14 08:59:41 +0800

    add HSSMS datasets

--- clsar/dataset/data.py
+++ clsar/dataset/data.py
@@ -7,6 +7,8 @@ Created on Wed Jun  8 10:40:35 2022
 
 import os
 import os.path as osp
+import pandas as pd
+import numpy as np
 import re
 from rdkit import Chem
 import torch
@@ -14,7 +16,6 @@ from torch_geometric.data import (InMemoryDataset, Data, download_url,
                                   extract_gz)
 
 
-    
 class LSSNS(InMemoryDataset):
     r"""The benchmark datasets of low-sample size narrow scaffold inhibitors, 
     containing inhibitors from differnt drug targets of GPCRs, Kinases, Nuclear Receptors, Proteases, Transporters, and Other Enzymes.
@@ -36,7 +37,7 @@ class LSSNS(InMemoryDataset):
             value, indicating whether the data object should be included in the
             final dataset. (default: :obj:`None`)
     """
-    
+
     url = 'https://raw.githubusercontent.com/bidd-group/MPCD/main/dataset/LSSNS/{}'
 
     # Format: name: [display_name, url_name, csv_name, smiles_idx, y_idx]
@@ -54,7 +55,7 @@ class LSSNS(InMemoryDataset):
              'mglur2': ['mGluR2', 'mGluR2.csv', 'mGluR2', 7, 12],
              'pkci': ['PKCi', 'PKCi.csv', 'PKCi', 3, 8],
              'eaat3': ['EAAT3', 'EAAT3.csv', 'EAAT3', 7, 12]}
-    
+
     def __init__(self, root, name, transform=None, pre_transform=None, pre_filter=None):
         self.name = name.lower()
         assert self.name in self.names.keys()
@@ -162,18 +163,17 @@ class LSSNS(InMemoryDataset):
 
     def __repr__(self) -> str:
         return f'{self.names[self.name][0]}({len(self)})'
-    
-    
+
     def smiles_to_data(self, smiles_list):
 
         data_list = []
         for i in range(len(smiles_list)):
             smiles = smiles_list[i]
             mol = Chem.MolFromSmiles(smiles)
-            mol = Chem.AddHs(mol) 
+            mol = Chem.AddHs(mol)
             if mol is None:
                 continue
-                
+
             xs = []
             for atom in mol.GetAtoms():
                 x = []
@@ -214,7 +214,7 @@ class LSSNS(InMemoryDataset):
                 perm = (edge_index[0] * x.size(0) + edge_index[1]).argsort()
                 edge_index, edge_attr = edge_index[:, perm], edge_attr[perm]
 
-            data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, 
+            data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr,
                         smiles=smiles)
 
             if self.pre_filter is not None and not self.pre_filter(data):
@@ -223,38 +223,11 @@ class LSSNS(InMemoryDataset):
             if self.pre_transform is not None:
                 data = self.pre_transform(data)
             data_list.append(data)
-            
+
         return data_list
 
 
-class ActivityCliff(InMemoryDataset):
-    r"""The benchmark datasets of low-sample size narrow scaffold inhibitors, 
-    containing inhibitors from differnt drug targets of GPCRs, Kinases, Nuclear Receptors, Proteases, Transporters, and Other Enzymes.
-    All datasets come with the additional node and edge features introduced by
-    the `Open Graph Benchmark <https://ogb.stanford.edu/docs/graphprop/>`_.
-    Args:
-        root (string): Root directory where the dataset should be saved.
-        name (string): The name of the dataset (:obj:`"mGluR2"`,
-            :obj:`"USP7"`, :obj:`"MTH1"`, :obj:`"RIP2"`, :obj:`"PKCi"`,
-            :obj:`"PHGDH"`, :obj:`"RORg"`, :obj:`"IDO1"`, :obj:`"KLK5"`,
-            :obj:`"Notum"`, :obj:`"EAAT3"`).
-        transform (callable, optional): A function/transform that takes in an
-            :obj:`torch_geometric.data.Data` object and returns a transformed
-            version. The data object will be transformed before every access.
-            (default: :obj:`None`)
-        pre_transform (callable, optional): A function/transform that takes in
-            an :obj:`torch_geometric.data.Data` object and returns a
-            transformed version. The data object will be transformed before
-            being saved to disk. (default: :obj:`None`)
-        pre_filter (callable, optional): A function that takes in an
-            :obj:`torch_geometric.data.Data` object and returns a boolean
-            value, indicating whether the data object should be included in the
-            final dataset. (default: :obj:`None`)
-    """
-    pass
-
-    
-class HSSMS(InMemoryDataset):
+class HSSMS(LSSNS):
     r"""The benchmark datasets of higher-sample size diversity scaffold inhibitors, 
     containing inhibitors from differnt drug targets of GPCRs, Kinases, Nuclear Receptors, Proteases, Transporters, and Other Enzymes.
     All datasets come with the additional node and edge features introduced by
@@ -278,10 +251,127 @@ class HSSMS(InMemoryDataset):
             value, indicating whether the data object should be included in the
             final dataset. (default: :obj:`None`)
     """
-    pass
-    
-    
+    url = 'https://raw.githubusercontent.com/bidd-group/MPCD/main/dataset/HSSMS/MoleculeACE_benchmark/{}'
+    meta = 'https://raw.githubusercontent.com/bidd-group/MPCD/main/dataset/HSSMS/MoleculeACE_benchmark/metadata/datasets.csv'
 
+    meta_table = pd.read_csv(meta)
+    # Format: name: [display_name, url_name, csv_name, smiles_idx, y_idx]
+    names = {}
+    for name in meta_table.Dataset.tolist():
+        names.update({name.lower(): [name, name + '.csv', name, 0, 2]})
+
+    def __init__(self, root, name, transform=None, pre_transform=None, pre_filter=None):
+        super().__init__(root, name, transform, pre_transform, pre_filter)
+
+    @property
+    def raw_dir(self):
+        return osp.join(self.root, self.name, 'raw')
+
+    @property
+    def processed_dir(self):
+        return osp.join(self.root, self.name, 'processed')
+
+    @property
+    def raw_file_names(self):
+        return f'{self.names[self.name][2]}.csv'
+
+    @property
+    def processed_file_names(self):
+        return 'data.pt'
+
+    def download(self):
+        url = self.url.format(self.names[self.name][1])
+        path = download_url(url, self.raw_dir)
+        if self.names[self.name][1][-2:] == 'gz':
+            extract_gz(path, self.raw_dir)
+            os.unlink(path)
+
+    def process(self):
+        from rdkit import Chem
+
+        with open(self.raw_paths[0], 'r', encoding="utf-8") as f:
+            dataset = f.read().split('\n')[1:-1]
+            dataset = [x for x in dataset if len(x) > 0]  # Filter empty lines.
+
+        data_list = []
+        for line in dataset:
+            line = re.sub(r'\".*\"', '', line)  # Replace ".*" strings.
+            line = line.split(',')
+
+            # smiles, raw_y, lgy, cliff, split
+            smiles, y_nm, y_nm_lg, cliff, split = line
+
+            cliff = int(cliff)
+            y_nm = float(y_nm)
+            y_nm_lg = float(y_nm_lg)
+            y_pm_lg = -np.log10(y_nm*1e-9)  # convert to pIC50
+
+            y_nm = torch.tensor([float(y_nm)], dtype=torch.float).view(1, -1)
+            y_nm_lg = torch.tensor(
+                [float(y_nm_lg)], dtype=torch.float).view(1, -1)
+            y = torch.tensor([float(y_pm_lg)], dtype=torch.float).view(1, -1)
+
+            mol = Chem.MolFromSmiles(smiles)
+            if mol is None:
+                continue
+
+            xs = []
+            for atom in mol.GetAtoms():
+                x = []
+                x.append(x_map['atomic_num'].index(atom.GetAtomicNum()))
+                x.append(x_map['chirality'].index(str(atom.GetChiralTag())))
+                x.append(x_map['degree'].index(atom.GetTotalDegree()))
+                x.append(x_map['formal_charge'].index(atom.GetFormalCharge()))
+                x.append(x_map['num_hs'].index(atom.GetTotalNumHs()))
+                x.append(x_map['num_radical_electrons'].index(
+                    atom.GetNumRadicalElectrons()))
+                x.append(x_map['hybridization'].index(
+                    str(atom.GetHybridization())))
+                x.append(x_map['is_aromatic'].index(atom.GetIsAromatic()))
+                x.append(x_map['is_in_ring'].index(atom.IsInRing()))
+                xs.append(x)
+
+            x = torch.tensor(xs, dtype=torch.long).view(-1, 9)
+
+            edge_indices, edge_attrs = [], []
+            for bond in mol.GetBonds():
+                i = bond.GetBeginAtomIdx()
+                j = bond.GetEndAtomIdx()
+
+                e = []
+                e.append(e_map['bond_type'].index(str(bond.GetBondType())))
+                e.append(e_map['stereo'].index(str(bond.GetStereo())))
+                e.append(e_map['is_conjugated'].index(bond.GetIsConjugated()))
+
+                edge_indices += [[i, j], [j, i]]
+                edge_attrs += [e, e]
+
+            edge_index = torch.tensor(edge_indices)
+            edge_index = edge_index.t().to(torch.long).view(2, -1)
+            edge_attr = torch.tensor(edge_attrs, dtype=torch.long).view(-1, 3)
+
+            # Sort indices.
+            if edge_index.numel() > 0:
+                perm = (edge_index[0] * x.size(0) + edge_index[1]).argsort()
+                edge_index, edge_attr = edge_index[:, perm], edge_attr[perm]
+
+            data = Data(x=x,
+                        edge_index=edge_index,
+                        edge_attr=edge_attr,
+                        y=y,
+                        cliff=cliff,
+                        split=split,
+                        y_nm=y_nm,
+                        y_nm_lg=y_nm_lg,
+                        smiles=smiles)
+
+            if self.pre_filter is not None and not self.pre_filter(data):
+                continue
+
+            if self.pre_transform is not None:
+                data = self.pre_transform(data)
+            data_list.append(data)
+        torch.save(self.collate(data_list), self.processed_paths[0])
 
 
 x_map = {
@@ -333,4 +423,3 @@ e_map = {
     ],
     'is_conjugated': [False, True],
 }
-    
